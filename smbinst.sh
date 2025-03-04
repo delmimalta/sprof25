aa59d8c9b0bb0b92ef4eeb64c0d310843a2299be
delmimalta <189194812+delmimalta@users.noreply.github.com>
2025-03-04 23:49:34 +0700

    Create smbinst.sh

---
+++ smbinst.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get install task-samba-dc -y
+control bind-chroot disabled
+grep -q KRB5RCACHETYPE /etc/sysconfig/bind || echo 'KRB5RCACHETYPE="none"' >> /etc/sysconfig/bind
+grep -q 'bind-dns' /etc/bind/named.conf || echo 'include "/var/lib/samba/bind-dns/named.conf";' >> /etc/bind/named.conf
+
+echo "DONE - CAN DO FURTHER"
