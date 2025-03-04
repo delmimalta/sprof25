327dc35a7d30eb7ffe948ddca90c431d550d7f1a
delmimalta <189194812+delmimalta@users.noreply.github.com>
2025-03-04 23:27:48 +0700

    Create cybackup.sh

---
+++ cybackup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get update && apt-get dist-upgrade -y && update-kernel -y && apt-get clean && reboot
+
+# apt-get install kernel-source-<x.x>
+# apt-get install kernel-headers-modules-std-def gcc make -y
+# chmod +x CyberBackup_17_64-bit.x86_64
+# ./CyberBackup_17_64-bit.x86_64
