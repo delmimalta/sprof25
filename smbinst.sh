be3a3624bae9aae0a19e5891eef7cfce60154a23
delmimalta <189194812+delmimalta@users.noreply.github.com>
2025-03-04 23:54:04 +0700

    Update smbinst.sh

--- smbinst.sh
+++ smbinst.sh
@@ -5,4 +5,5 @@ control bind-chroot disabled
 grep -q KRB5RCACHETYPE /etc/sysconfig/bind || echo 'KRB5RCACHETYPE="none"' >> /etc/sysconfig/bind
 grep -q 'bind-dns' /etc/bind/named.conf || echo 'include "/var/lib/samba/bind-dns/named.conf";' >> /etc/bind/named.conf
 
+
 echo "DONE - CAN DO FURTHER"
