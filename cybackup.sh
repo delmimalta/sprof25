e6fb25e09a65bfe4e2dc7d8c466443b4cbc5e333
delmimalta <189194812+delmimalta@users.noreply.github.com>
2025-03-05 00:27:12 +0700

    Update cybackup.sh

--- cybackup.sh
+++ cybackup.sh
@@ -2,14 +2,15 @@
 
 apt-get update && apt-get dist-upgrade -y && update-kernel -y && apt-get clean && reboot
 
-
+echo ""
 echo "DONE - UNCOMMENT AFTER REBOOT"
+echo ""
 
+#apt-get install kernel-source-<x.x>
+#apt-get install kernel-headers-modules-std-def gcc make -y
+#chmod +x CyberBackup_17_64-bit.x86_64
+#./CyberBackup_17_64-bit.x86_64
 
-# apt-get install kernel-source-<x.x>
-# apt-get install kernel-headers-modules-std-def gcc make -y
-# chmod +x CyberBackup_17_64-bit.x86_64
-# ./CyberBackup_17_64-bit.x86_64
-
-
-# echo "DONE - CAN DO FURTHER"
+#echo ""
+#echo "DONE - CAN DO FURTHER"
+#echo ""
