e454554c7c964cab677e99202d1e5a1faaac5d54
delmimalta <189194812+delmimalta@users.noreply.github.com>
2025-03-06 06:22:03 +0700

    Update loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -25,22 +25,22 @@ resource "openstack_lb_loadbalancer_v2" "loadbalancer" {
 # Создаём правило в балансировщике нагрузки для HTTP
 
 
-resource "openstack_lb_listener_v2" "listener_http" {
-  name            = "HTTP"
-  protocol        = "TCP"
-  protocol_port   = "80"
-  loadbalancer_id = openstack_lb_loadbalancer_v2.loadbalancer.id
-}
+#resource "openstack_lb_listener_v2" "listener_http" {
+#  name            = "HTTP"
+#  protocol        = "TCP"
+#  protocol_port   = "80"
+#  loadbalancer_id = openstack_lb_loadbalancer_v2.loadbalancer.id
+#}
 
 # Создаём правило в балансировщике нагрузки для HTTPS
 
 
-resource "openstack_lb_listener_v2" "listener_https" {
-  name            = "HTTPS"
-  protocol        = "TCP"
-  protocol_port   = "443"
-  loadbalancer_id = openstack_lb_loadbalancer_v2.loadbalancer.id
-}
+#resource "openstack_lb_listener_v2" "listener_https" {
+#  name            = "HTTPS"
+#  protocol        = "TCP"
+#  protocol_port   = "443"
+#  loadbalancer_id = openstack_lb_loadbalancer_v2.loadbalancer.id
+#}
 
 
 
@@ -56,22 +56,22 @@ resource "openstack_lb_listener_v2" "listener_https" {
 # Создаём целевую группу для HTTP с указанием алгоритма балансировки "ROUND ROBIN"
 
 
-resource "openstack_lb_pool_v2" "pool_http" {
-  name        = "HTTP"
-  protocol    = "HTTP"
-  lb_method   = "ROUND_ROBIN"
-  listener_id = openstack_lb_listener_v2.listener_http.id
-}
+#resource "openstack_lb_pool_v2" "pool_http" {
+#  name        = "HTTP"
+#  protocol    = "HTTP"
+#  lb_method   = "ROUND_ROBIN"
+#  listener_id = openstack_lb_listener_v2.listener_http.id
+#}
 
 # Создаём целевую группу для HTTPS с указанием алгоритма балансировки "ROUND ROBIN"
 
 
-resource "openstack_lb_pool_v2" "pool_https" {
-  name        = "HTTPS"
-  protocol    = "HTTPS"
-  lb_method   = "ROUND_ROBIN"
-  listener_id = openstack_lb_listener_v2.listener_https.id
-}
+#resource "openstack_lb_pool_v2" "pool_https" {
+#  name        = "HTTPS"
+#  protocol    = "HTTPS"
+#  lb_method   = "ROUND_ROBIN"
+#  listener_id = openstack_lb_listener_v2.listener_https.id
+#}
 
 
 
@@ -87,46 +87,46 @@ resource "openstack_lb_pool_v2" "pool_https" {
 # Добавляем инстанс WEB1  в целевую группу HTTP
 
 
-resource "openstack_lb_member_v2" "member_web1_http" {
-  name          = "WEB1"
-  subnet_id     = openstack_networking_subnet_v2.subnet.id
-  pool_id       = openstack_lb_pool_v2.pool_http.id
-  address       = "192.168.200.21"
-  protocol_port = "80"
-}
+#resource "openstack_lb_member_v2" "member_web1_http" {
+#  name          = "WEB1"
+#  subnet_id     = openstack_networking_subnet_v2.subnet.id
+#  pool_id       = openstack_lb_pool_v2.pool_http.id
+#  address       = "192.168.200.21"
+#  protocol_port = "80"
+#}
 
 # Добавляем инстанс WEB2  в целевую группу HTTP
 
 
-resource "openstack_lb_member_v2" "member_web2_http" {
-  name          = "WEB2"
-  subnet_id     = openstack_networking_subnet_v2.subnet.id
-  pool_id       = openstack_lb_pool_v2.pool_http.id
-  address       = "192.168.200.22"
-  protocol_port = "80"
-}
+#resource "openstack_lb_member_v2" "member_web2_http" {
+#  name          = "WEB2"
+#  subnet_id     = openstack_networking_subnet_v2.subnet.id
+#  pool_id       = openstack_lb_pool_v2.pool_http.id
+#  address       = "192.168.200.22"
+#  protocol_port = "80"
+#}
 
 # Добавляем инстанс WEB1  в целевую группу HTTPS
 
 
-resource "openstack_lb_member_v2" "member_web1_https" {
-  name          = "WEB1"
-  subnet_id     = openstack_networking_subnet_v2.subnet.id
-  pool_id       = openstack_lb_pool_v2.pool_https.id
-  address       = "192.168.200.21"
-  protocol_port = "80"
-}
+#resource "openstack_lb_member_v2" "member_web1_https" {
+#  name          = "WEB1"
+#  subnet_id     = openstack_networking_subnet_v2.subnet.id
+#  pool_id       = openstack_lb_pool_v2.pool_https.id
+#  address       = "192.168.200.21"
+#  protocol_port = "80"
+#}
 
 # Добавляем инстанс WEB2  в целевую группу HTTPS
 
 
-resource "openstack_lb_member_v2" "member_web2_https" {
-  name          = "WEB2"
-  subnet_id     = openstack_networking_subnet_v2.subnet.id
-  pool_id       = openstack_lb_pool_v2.pool_https.id
-  address       = "192.168.200.22"
-  protocol_port = "80"
-}
+#resource "openstack_lb_member_v2" "member_web2_https" {
+#  name          = "WEB2"
+#  subnet_id     = openstack_networking_subnet_v2.subnet.id
+#  pool_id       = openstack_lb_pool_v2.pool_https.id
+#  address       = "192.168.200.22"
+#  protocol_port = "80"
+#}
 
 
 
@@ -142,23 +142,23 @@ resource "openstack_lb_member_v2" "member_web2_https" {
 # Создаём проверку доступности инстансов в целевой группе HTTP
 
 
-resource "openstack_lb_monitor_v2" "monitor_http" {
-  name        = "monitor HTTP"
-  pool_id     = openstack_lb_pool_v2.pool_http.id
-  type        = "PING"
-  delay       = "10"
-  timeout     = "4"
-  max_retries = "5"
-}
+#resource "openstack_lb_monitor_v2" "monitor_http" {
+#  name        = "monitor HTTP"
+#  pool_id     = openstack_lb_pool_v2.pool_http.id
+#  type        = "PING"
+#  delay       = "10"
+#  timeout     = "4"
+#  max_retries = "5"
+#}
 
 # Создаём проверку доступности инстансов в целевой группе HTTPS
 
 
-resource "openstack_lb_monitor_v2" "monitor_https" {
-  name        = "monitor HTTPS"
-  pool_id     = openstack_lb_pool_v2.pool_https.id
-  type        = "PING"
-  delay       = "10"
-  timeout     = "4"
-  max_retries = "5"
-}
+#resource "openstack_lb_monitor_v2" "monitor_https" {
+#  name        = "monitor HTTPS"
+#  pool_id     = openstack_lb_pool_v2.pool_https.id
+#  type        = "PING"
+#  delay       = "10"
+#  timeout     = "4"
+#  max_retries = "5"
+#}
