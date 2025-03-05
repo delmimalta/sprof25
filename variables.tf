ca6725b63ae74b38ca073e54867c3c032f4f94ec
delmimalta <189194812+delmimalta@users.noreply.github.com>
2025-03-06 05:54:51 +0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "OS_AUTH_URL" {
+	type = string
+	sensitive = true
+}
+
+variable "OS_PROJECT_NAME" {
+	type = string
+	sensitive = true
+}
+
+variable "OS_USERNAME" {
+	type = string
+	sensitive = true
+}
+
+variable "OS_PASSWORD" {
+	type = string
+	sensitive = true
+}
